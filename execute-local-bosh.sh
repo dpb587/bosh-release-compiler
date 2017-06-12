59fcd9c01dc35529086ea73e6a9b50bae92db079
Danny Berger <dpb587@gmail.com>
2017-06-11 22:03:40 -0700

    Tear down local director

--- execute-local-bosh.sh
+++ execute-local-bosh.sh
@@ -16,3 +16,15 @@ bosh -n update-cloud-config \
   )
 
 "$DIR/execute.sh"
+
+bosh -n clean-up --all
+
+set +u
+
+source /etc/profile.d/chruby.sh
+
+chruby 2.3.1
+
+bosh delete-env "/tmp/local-bosh/director/bosh-director.yml" \
+  --vars-store="/tmp/local-bosh/director/creds.yml" \
+  --state="/tmp/local-bosh/director/state.json"
