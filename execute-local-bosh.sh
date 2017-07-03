8f6d5fd8e2a38eefe480ae8f953ceb6b8a00b0f2
Danny Berger <dpb587@gmail.com>
2017-07-02 18:52:34 -0700

    Avoid chruby

--- execute-local-bosh.sh
+++ execute-local-bosh.sh
@@ -19,12 +19,6 @@ bosh -n update-cloud-config \
 
 bosh -n clean-up --all
 
-set +u
-
-source /etc/profile.d/chruby.sh
-
-chruby 2.3.1
-
 bosh delete-env "/tmp/local-bosh/director/bosh-director.yml" \
   --vars-store="/tmp/local-bosh/director/creds.yml" \
   --state="/tmp/local-bosh/director/state.json"
